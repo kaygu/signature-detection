ad05b44a3d22a3f3d1ccf6719014d39e0e9b25b9
kaygu <camille.deneef@gmail.com>
2021-10-19 16:49:56 +0200

    map all xml & save to npy file

--- parse_train.py
+++ parse_train.py
@@ -1,27 +1,39 @@
 import os
 from bs4 import BeautifulSoup
-from PIL import Image
+import cv2
+import numpy as np
 
 if __name__ == '__main__':
-  signature_counter = 0
-  file_counter = 0
+  IMAGE_SIZE = 128
 
   dir_path = './data/train_xml/'
   files = os.listdir(dir_path)
-  for file_name in files[:5]:
-    # print(file_name)
+  img_list = []
+  label_list = []
+  for file_name in files:
     with open(dir_path + file_name, 'r') as file:
       content = file.read()
       bs_content = BeautifulSoup(content, 'lxml')
       query = bs_content.find_all("dl_zone", {"gedi_type": "DLSignature"})
-      for result in query:
-        x_min = int(result['col'])
-        x_max = x_min + int(result['width'])
-        y_min = int(result['row'])
-        y_max = y_min+ int(result['height'])
-        print(f"{x_min}, {y_min}, {x_max}, {y_max}")
-        im = Image.open('data/train/'+file_name[:-4] + '.tif')
-        im_crop = im.crop((x_min, y_min, x_max, y_max))
-        im_crop.show()
+      img = cv2.imread('data/train/'+file_name[:-4] + '.tif', cv2.IMREAD_GRAYSCALE)
+      cv2.resize(img, (IMAGE_SIZE, IMAGE_SIZE))
+      img_list.append(img)
+      label_list.append(1 if query else 0)
+      # for result in query:
+      #   # Get Signature boundaries
+      #   x_min = int(result['col'])
+      #   x_max = x_min + int(result['width'])
+      #   y_min = int(result['row'])
+      #   y_max = y_min+ int(result['height'])
+      #   # Crop img
+      #   sig = img[y_min:y_max, x_min:x_max]
+      #   sig = cv2.resize(sig, (IMAGE_SIZE, IMAGE_SIZE))
+      #   cv2.imshow("signature", sig)
+      #   cv2.waitKey(0)
+
+  label_array = np.array(label_list)
+  img_array = np.array(img_list)
+  np.save('img.npy', img_array, allow_pickle=True)
+  np.save('label.npy', label_array, allow_pickle=True)
 
   
